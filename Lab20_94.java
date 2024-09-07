3742c2e4183c7ac8fd61c538213900cb40a45906
Varunpattani <160588139+Varunpattani@users.noreply.github.com>
2024-09-07 10:48:52 +0530

    Update Lab20_94.java

--- Lab20_94.java
+++ Lab20_94.java
@@ -2,7 +2,7 @@ public class Lab20_94 {
     public static void main(String[] args) {
         int[] arr = {3,2,1,5,4};
         int index = 0;
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j<arr.length;j++){
                 if(arr[i] > arr[j]) index = j;
             }
